f952d862c97f5cda2505d560c2d9a49bcb9f31d3
Akash Thakur <akashthakur5267@gmail.com>
2025-08-05 19:56:12 +0530

    Add new feature

---
+++ app.js
@@ -0,0 +1,34 @@
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("button");
+
+// btn.addEventListener('click',()=>{
+
+//     let country = document.querySelector('input').value;
+//     console.log(country);
+
+//     let colArr = getCollege(country);
+//     Show(colArr);
+// });
+
+// function Show(colArr){
+//       let list = document.querySelector('#list');
+//       list.innerText = "";
+//       for(col of colArr){
+//             console.log(col.name);
+
+//             let li = document.createElement('li');
+//             li.innerText = col.name;
+//             list.appendChild(li);
+//       }
+// };
+country = "nepal";
+async function getCollege(country) {
+    try{
+        let res = await axios.get(url + country);
+        return res.data;
+    }catch(e){
+        console.log("Error:",e);
+        return [];
+    }
+    
+}
